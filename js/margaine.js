81ae4c6d54250a2dfcd292959418bf4540d6087a
unknown <Propritaire@.(none)>
2012-02-03 16:57:28 +0100

    Back to top button :-)

---
+++ js/margaine.js
@@ -0,0 +1,17 @@
+(function($) {
+  /**
+   * Scripts spécifiques à margaine.com
+   */
+  // Back to top link
+  $(window).scroll(function() {
+    if($(this).scrollTop() != 0) {
+      $('#toTop').fadeIn();	
+    } else {
+      $('#toTop').fadeOut();
+    }
+  });
+
+  $('#toTop').click(function() {
+    $('body,html').animate({scrollTop:0},800);
+  });
+}(jQuery));
